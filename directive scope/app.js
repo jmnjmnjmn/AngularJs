25d9524cbb75909540e4451abfc77c2fb1e96939
jmnjmnjmn <jiamengnan@gmail.com>
2016-04-24 15:53:53 -0700

    directive transclude

--- directive scope/app.js
+++ directive scope/app.js
@@ -70,18 +70,6 @@ myApp.directive("searchResult", function() {
            personObject: "=",
            formattedAddressFunction: "&"
        },
-       link: function(scope, elements, attrs) {
-                   
-           console.log('Linking...');
-
-           console.log(scope);
-
-           if (scope.personObject.name == 'Jane Doe') {
-                elements.removeAttr('class');
-           }
-
-           console.log(elements);
-                   
-        }           
+       transclude: true
    }
 });
