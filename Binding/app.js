2a6d99cf4233cc46cc1a9348621018d0ec29c2d2
jmnjmnjmn <jiamengnan@gmail.com>
2016-04-23 20:39:15 -0700

    directive

--- Binding/app.js
+++ Binding/app.js
@@ -8,4 +8,16 @@ myApp.controller('mainController', ['$scope', '$filter', function($scope, $filte
         return $filter('lowercase')($scope.handle);
     };
     
+    $scope.characters = 5;
+    
+    $scope.rules = [
+      
+        { rulename: "Must be 5 characters" },
+        { rulename: "Must not be used elsewhere" },
+        { rulename: "Must be cool" }
+        
+    ];
+    
+    console.log($scope.rules);
+    
 }]);
