13b4c88c8dbd447016744d7222e02d7d4f0d43bb
jmnjmnjmn <jiamengnan@gmail.com>
2016-04-24 20:38:00 -0700

    custom directive and split

---
+++ Weather/controllers.js
@@ -0,0 +1,34 @@
+// CONTROLLERS
+weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+    
+    $scope.city = cityService.city;
+    
+    $scope.$watch('city', function() {
+       cityService.city = $scope.city; 
+    });
+    
+}]);
+
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
+    
+    $scope.city = cityService.city;
+    
+    $scope.days = $routeParams.days || '2';
+    
+    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?appid=2848488221f524605647a78f1ed5b280", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+    
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
+    
+    $scope.convertToFahrenheit = function(degK) {
+        
+        return Math.round((1.8 * (degK - 273)) + 32);
+        
+    }
+    
+    $scope.convertToDate = function(dt) { 
+      
+        return new Date(dt * 1000);
+        
+    };
+    
+}]);
